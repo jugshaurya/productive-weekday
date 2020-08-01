51ffccb34ef494159f729b0fae396f089ac3c1de
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2020-08-01 14:49:50 +0530

    refactor get dataset of user

--- visuald3server/api/getuserInfo.js
+++ visuald3server/api/getuserInfo.js
@@ -3,7 +3,6 @@ const fetch = require("node-fetch");
 const getUserInfo = async user => {
   const userName = encodeURI(user.toLowerCase());
   const userGithubUrl = `https://api.github.com/users/${userName}`;
-
   const response = await fetch(userGithubUrl);
 
   if (response.status >= 400) {
@@ -13,14 +12,15 @@ const getUserInfo = async user => {
   }
 
   const result = await response.json();
+  const {avatar_url, name, login, created_at} = result;
 
   return {
-    avatar_url: result.avatar_url,
-    name: result.name,
-    github_username: result.login,
+    avatar_url,
+    name,
+    github_username: login,
     // return only the year ex: 2018-06-01T08:50:23Z
-    joinedYear: result.created_at.split("-")[0],
-    joinedDate: result.created_at.split("T")[0]
+    joinedYear: created_at.split("-")[0],
+    joinedDate: created_at.split("T")[0]
   };
 };
 
