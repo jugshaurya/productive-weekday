4bca901eca6da27bfa92d9e6997116849cd1f7ee
jugshaurya <shauryasinghal84@gmail.com>
2026-04-19 15:44:46 +0530

    fix: remove priyansh18 from CORS whitelist

--- visuald3server/server.js
+++ visuald3server/server.js
@@ -20,7 +20,6 @@ var whitelist = [
   "http://localhost:3000", // for react apps
   "http://localhost:8000", // for gatsby app
   "http://localhost:8080", // for local server setup
-  "https://www.priyanshsinghal.com", // for requested User - Priyansh Singhal :) - priyansh18
   "https://showcase-jugshaurya.vercel.app", // new portfolio website
   "https://showcase-three.vercel.app", // new portfolio website
   "https://jugshaurya.vercel.app", // new portfolio website
